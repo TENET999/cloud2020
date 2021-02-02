879f1c21f795fcdf50a35518bef063190fa4ceef
云销雨霁 <lzw8023@foxmail.com>
2021-02-02 08:55:43 +0800

    sentinel热点

--- cloudalibaba-sentinel-service8401/src/main/java/com/lzw/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lzw/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,7 +1,10 @@
 package com.lzw.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
@@ -44,6 +47,22 @@ public class FlowLimitController {
         }
         log.info(Thread.currentThread().getName() + "请求testD");
         return "------testD";
+
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1", required = false)String p1,
+                             @RequestParam(value = "p1", required = false)String p2){
+
+        return "------testHotKey";
+
+    }
+
+    // 自己的兜底方法
+    public String deal_testHotKey(String p1, String p2, BlockException exception){
+
+        return "------deal_testHotKey, o(╥﹏╥)o";    // sentinel系统默认的提示：Block by Sentinel（flow limiting）
 
     }
 
